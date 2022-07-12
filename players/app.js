607659f663b0d705dd649f0de2f7e3d88d3f8b0a
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-07-12 09:51:06 -0700

    Implement players list

--- players/app.js
+++ players/app.js
@@ -3,18 +3,23 @@ import { protectPage } from '../utils.js';
 
 // Services
 import { getUser, signOut } from '../services/auth-service.js';
+import { getPlayers } from '../services/league-service.js';
 
 // Components
 import createUser from '../components/User.js';
 import createNavigation from '../components/Navigation.js';
+import createPlayerList from '../components/PlayerList.js';
 
 // State
 let user = null;
+let players = [];
 
 // Action Handlers
 async function handlePageLoad() {
     user = getUser();
     if (protectPage(user)) return;
+
+    players = await getPlayers() ?? [];
 
     display();
 }
@@ -31,9 +36,12 @@ const User = createUser(
 
 const Navigation = createNavigation(document.querySelector('#nav'));
 
+const PlayerList = createPlayerList(document.querySelector('#player-list'));
+
 function display() {
     User({ user });
     Navigation();
+    PlayerList({ players });
 }
 
 handlePageLoad();
